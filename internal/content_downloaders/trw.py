59a3f82014e4c69475e7bde31e76271f97e442e9
Mustansir Muzaffar <mustansir2001@gmail.com>
2025-06-25 22:53:36 +0500

    fix

--- internal/content_downloaders/trw.py
+++ internal/content_downloaders/trw.py
@@ -12,7 +12,8 @@ from internal.content_downloaders.exceptions import AuthenticationError
 from internal.content_downloaders.types import Content
 from internal.utils import sanitize
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 REQUEST_HEADERS = {
@@ -43,6 +44,7 @@ SERVERS = [
 
 DOWNLOAD_DIR = "downloads/trw/"
 
+
 class TRWContentDownloader:
 
     def __init__(self, email: str, password: str):
@@ -60,9 +62,8 @@ class TRWContentDownloader:
         self.auth_headers = REQUEST_HEADERS.copy()
         self.auth_headers['x-session-token'] = response.json()['token']
 
-    
     def get_content(self) -> Generator[Content, None, None]:
-        clear_download_dir() # Clear the download directory before starting
+        clear_download_dir()  # Clear the download directory before starting
         for server_name, server_id in SERVERS:
             server_data = self.fetch_server_data(server_id)
             heirarchy = [("servers", server_name)]
@@ -71,34 +72,46 @@ class TRWContentDownloader:
             modules_lookup = server_data["modules_lookup"]
             for category_id in server_data["categories"]:
                 category = categories_lookup[category_id]
-                heirarchy_category = heirarchy + [("categories", category["title"])]
+                heirarchy_category = heirarchy + \
+                    [("categories", category["title"])]
                 for course_num, course_id in enumerate(category["courses"], start=1):
                     course = courses_lookup[course_id]
-                    heirarchy_course = heirarchy_category + [("courses", f'{course_num}. {course["title"]}')]
+                    heirarchy_course = heirarchy_category + \
+                        [("courses", f'{course_num}. {course["title"]}')]
                     if course["embed_link"]:
                         file_path = DOWNLOAD_DIR + f'{course["title"]}.html'
-                        download_embed_link(course["embed_link"], file_path)
-                        yield Content(
-                            file_type="html",
-                            name=f'{course["title"]}.html',
-                            path=file_path,
-                            hierarchy=heirarchy_course
-                        )
-                        delete_media(file_path)
+                        try:
+                            download_embed_link(
+                                course["embed_link"], file_path)
+                            yield Content(
+                                file_type="html",
+                                name=f'{course["title"]}.html',
+                                path=file_path,
+                                hierarchy=heirarchy_course
+                            )
+                            delete_media(file_path)
+                        except Exception as e:
+                            logging.error(
+                                f"Error downloading embed link for course {course['title']}: {str(e)}")
+                            continue
                     for module_num, module_id in enumerate(course["modules"], start=1):
                         module = modules_lookup[module_id]
-                        heirarchy_module = heirarchy_course + [("modules", f'{module_num}. {module["title"]}')]
+                        heirarchy_module = heirarchy_course + \
+                            [("modules", f'{module_num}. {module["title"]}')]
                         for lesson_num, lesson_id in enumerate(module["lessons"], start=1):
                             try:
                                 lesson_data = self.fetch_lesson_data(lesson_id)
-                                heirarchy_lesson = heirarchy_module + [("lessons", f'{lesson_num}. {lesson_data["title"]}')] 
+                                heirarchy_lesson = heirarchy_module + \
+                                    [("lessons",
+                                      f'{lesson_num}. {lesson_data["title"]}')]
                                 for field in lesson_data["form"]["fields"]:
                                     if field.get("attachment") and field["attachment"].get("properties") and field["attachment"]["properties"].get("downloadUrl"):
                                         download_url = field["attachment"]["properties"]["downloadUrl"]
                                         title = field["title"]
                                         if not title:
                                             title = lesson_data["title"]
-                                        title = sanitize(title)  # Sanitize title for filename
+                                        # Sanitize title for filename
+                                        title = sanitize(title)
                                         filename = f'{lesson_num}. {title}.mp4'
                                         path = DOWNLOAD_DIR + filename
                                         download_video(download_url, path)
@@ -108,8 +121,10 @@ class TRWContentDownloader:
                                             path=path,
                                             hierarchy=heirarchy_lesson
                                         )
-                                        delete_media(path)  # Delete after yielding to save space
-                                        field["attachment"] = {"type": "video", "file": filename}
+                                        # Delete after yielding to save space
+                                        delete_media(path)
+                                        field["attachment"] = {
+                                            "type": "video", "file": filename}
                                 with open(DOWNLOAD_DIR + "lesson_data.json", "w") as f:
                                     json.dump(lesson_data, f, indent=4)
                                 yield Content(
@@ -118,24 +133,23 @@ class TRWContentDownloader:
                                     path=DOWNLOAD_DIR + "lesson_data.json",
                                     hierarchy=heirarchy_lesson
                                 )
-                                delete_media(DOWNLOAD_DIR + "lesson_data.json")  # Delete after yielding to save space
+                                # Delete after yielding to save space
+                                delete_media(DOWNLOAD_DIR + "lesson_data.json")
                             except Exception as e:
-                                logging.error(f"Error fetching lesson: {str(e)}")
+                                logging.error(
+                                    f"Error fetching lesson: {str(e)}")
                                 continue
 
-
-    
     def fetch_server_data(self, server_id: str) -> Dict[str, any]:
         url = f"https://rpc.therealworld.ag/api/trpc/school.fetchServerDataCached?input=%7B%22serverId%22%3A%22{server_id}%22%7D"
         response = self.__request("GET", url, auth=True)
         return response.json()["result"]["data"]
-    
+
     def fetch_lesson_data(self, lesson_id: str) -> Dict[str, any]:
         url = f"https://rpc.therealworld.ag/api/trpc/school.retrieveLessonSecure?input=%7B%22lessonId%22%3A%22{lesson_id}%22%7D"
         response = self.__request("GET", url, auth=True)
         encrypted_value = response.json()["result"]["data"]["value"]
         return decrypt_lesson(encrypted_value, self.auth_headers['x-session-token'])
-
 
     def __request(self, method: str, url: str, data: dict = None, auth: bool = False) -> requests.Response:
         if auth:
@@ -148,7 +162,8 @@ class TRWContentDownloader:
         response = requests.request(method, url, headers=headers, json=data)
 
         if not response.ok:
-            raise AuthenticationError(f"Request failed with status code {response.status_code}: {response.text}")
+            raise AuthenticationError(
+                f"Request failed with status code {response.status_code}: {response.text}")
 
         return response
 
@@ -164,7 +179,8 @@ def decrypt_lesson(encrypted: str, session_token: str) -> dict:
     ciphertext = binascii.unhexlify(data_hex)
 
     # AES-CBC decryption
-    cipher = Cipher(algorithms.AES(key_bytes), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key_bytes),
+                    modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     decrypted_padded = decryptor.update(ciphertext) + decryptor.finalize()
 
@@ -174,7 +190,6 @@ def decrypt_lesson(encrypted: str, session_token: str) -> dict:
 
     # Decode JSON
     return json.loads(decrypted.decode("utf-8"))
-
 
 
 def download_video(url: str, output_path: str) -> None:
@@ -199,12 +214,12 @@ def clear_download_dir() -> None:
                 os.remove(file_path)
 
 
-
 def download_embed_link(embed_link: str, output_path: str) -> None:
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     res = requests.get(embed_link, headers=REQUEST_HEADERS)
     if res.status_code != 200:
-        raise Exception(f"Failed to download embed link: {res.status_code} - {res.text}")
+        raise Exception(
+            f"Failed to download embed link: {res.status_code} - {res.text}")
     # save as html
     with open(output_path, 'wb') as f:
         f.write(res.content)
